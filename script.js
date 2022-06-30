0510f1a661db40031df498999c276fe4a345cf43
anthonytruongg <anthonytruongg1@gmail.com>
2022-06-30 12:11:44 -0700

    Display books function working

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 let body = document.body
 
-const Book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
-const Book2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 463);
-const Book3 = new Book('The Avengers', 'Marvel', 542);
+// const Book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
+// const Book2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 463);
+// const Book3 = new Book('The Avengers', 'Marvel', 542);
 
-let myLibrary = [Book1, Book2, Book3];
+// let myLibrary = [Book1, Book2, Book3];
+let myLibrary = []
 
 function Book(title, author, pages) {
     this.title = title;
@@ -14,53 +15,73 @@ function Book(title, author, pages) {
 
     // constructor
 }
+// -------------------------------------------------//
+const libraryGrid = document.querySelector('.libraryGrid');
 
+// const addBook = document.querySelector('.addBook');
+// addBook.addEventListener('click',createBook);
+
+// this function displays the input form
+function createBook() {
+   const displayForm = document.querySelector('.inputForm');
+    displayForm.style.display = 'contents';
+
+    body.append(displayForm);
+}
+
+const submitBook = document.querySelector('.submit')
+submitBook.addEventListener('click', addBookToLibrary);
+
+
+// this function adds a book the the library
 function addBookToLibrary() {
-    let title = prompt('Title:');
-    let author = prompt('Author:');
-    let pages = prompt('Pages:');
-    let newBook = new Book(title, author, pages);
+    const inputTitle = document.getElementById("title").value;
+    const inputAuthor = document.getElementById("author").value;
+    const inputPages = document.getElementById("pages").value;
+
+    let newBook = new Book(inputTitle, inputAuthor, inputPages);
+    
     myLibrary.push(newBook);
 }
 
-Book.prototype.read = function () {
-    return true
+
+
+function displayBook() {
+   
+    // const book = document.createElement('div');
+    // book.classList.add('bookCard');
+
+    // const readButton = document.createElement('button');
+    // readButton.classList.add('readButton');
+    // const removeButton = document.createElement('button');
+    // removeButton.classList.add('removeButton');
+    if(myLibrary.length == 0) {
+        return 0;
+    }
+    else {
+        for(let i = 0; i < myLibrary.length; i++) {
+            const book = document.createElement('div');
+            book.classList.add('bookCard');
+    
+            const readButton = document.createElement('button');
+            readButton.classList.add('readButton');
+            const removeButton = document.createElement('button');
+            removeButton.classList.add('removeButton');
+    
+            const bookTitle = document.createElement('h3');
+            bookTitle.textContent = `${myLibrary[i].title}`;
+            const bookAuthor = document.createElement('p');
+            bookAuthor.textContent = `${myLibrary[i].author}`;
+            const bookPages = document.createElement('p');
+            bookPages.textContent = `${myLibrary[i].pages}`;
+    
+            book.append(bookTitle, bookAuthor, bookPages, readButton, removeButton);
+            libraryGrid.append(book);
+        }
+    }
+    
+    
+    
 }
 
-Book.prototype.info = function () {
-    displayInfo.textContent = `${this.title} by ${this.author} with ${this.pages} pages`;
-    console.log(`${this.title} by ${this.author} with ${this.pages} pages`)
-}
-const displayInfo = document.querySelector('.bookInfo')
-
-// function displayBooks() {
-//     for (let i = 0; i < myLibrary.length; i++) {
-        
-//         displayInfo.textContent = myLibrary[i];
-        
-//         console.log(`${i + 1}. ${myLibrary[i].title} by ${myLibrary[i].author} with ${myLibrary[i].pages} pages`)
-//     }
-// }
-
-// const displayBook = document.querySelector('.displayBooks');
-// displayBook.addEventListener('click', displayBooks);
-
-
-
-// -------------------------------------------------//
-const addBook = document.querySelector('.addBook');
-addBook.addEventListener('click',addBookToLibrary, createBook);
-
-const libraryGrid = document.querySelector('.libraryGrid');
-function createBook() {
-    const book = document.createElement('div');
-    book.classList.add('bookCard');
-
-    const readButton = document.createElement('button');
-    readButton.classList.add('readButton');
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('removeButton');
-
-    book.append(readButton, removeButton);
-    libraryGrid.append(book);
-}
+displayBook()
